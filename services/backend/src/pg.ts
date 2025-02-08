d4b862d654afaf79fe14c384d1027a2c886e6331
elliotnn <elliot@norrevik.se>
2025-02-08 12:28:06 +0100

    Removed backend dir

--- services/backend/src/pg.ts
+++
@@ -1,8 +0,0 @@
-import knex from "knex";
-
-import config from "./knexfile";
-
-const environment = process.env.NODE_ENV || "development";
-const pg = knex(config[environment]);
-
-export default pg;
